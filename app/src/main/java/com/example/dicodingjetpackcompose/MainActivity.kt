30408668fd906c1610c9439795eccb52b0eedb12
gadogado092 <gadogado092>
2022-11-19 23:07:10 +0800

    add category

--- app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
@@ -4,16 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.dicodingjetpackcompose.model.dummyCategory
+import com.example.dicodingjetpackcompose.ui.components.CategoryItem
 import com.example.dicodingjetpackcompose.ui.components.SearchBar
+import com.example.dicodingjetpackcompose.ui.components.SectionText
 import com.example.dicodingjetpackcompose.ui.theme.DicodingJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -31,6 +35,8 @@ class MainActivity : ComponentActivity() {
 fun JetCoffeeApp() {
     Column {
         Banner()
+        SectionText(stringResource(R.string.section_category))
+        CategoryRow()
     }
 }
 
@@ -48,6 +54,29 @@ fun Banner(
         SearchBar()
     }
 }
+
+@Composable
+fun CategoryRow(
+    modifier: Modifier = Modifier
+) {
+    LazyRow(
+        horizontalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(horizontal = 16.dp),
+        modifier = modifier
+    ) {
+        items(dummyCategory, key = { it.textCategory }) { category ->
+            CategoryItem(category)
+        }
+    }
+}
+
+//@Preview(showBackground = true)
+//@Composable
+//fun CategoryRowPreview() {
+//    DicodingJetpackComposeTheme {
+//        CategoryRow()
+//    }
+//}
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
