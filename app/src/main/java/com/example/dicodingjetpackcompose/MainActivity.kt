1d6f62e25977acef6c900e7dc77b53690d44ee93
gadogado092 <gadogado092>
2022-11-19 22:48:10 +0800

    add banner

--- app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
@@ -3,13 +3,17 @@ package com.example.dicodingjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.dicodingjetpackcompose.ui.components.SearchBar
 import com.example.dicodingjetpackcompose.ui.theme.DicodingJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +21,38 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DicodingJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                JetCoffeeApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun JetCoffeeApp() {
+    Column {
+        Banner()
+    }
 }
 
-@Preview(showBackground = true)
+@Composable
+fun Banner(
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        Image(
+            painter = painterResource(R.drawable.banner),
+            contentDescription = "Banner Image",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.height(160.dp)
+        )
+        SearchBar()
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun DefaultPreview() {
     DicodingJetpackComposeTheme {
-        Greeting("Android")
+        JetCoffeeApp()
     }
 }
