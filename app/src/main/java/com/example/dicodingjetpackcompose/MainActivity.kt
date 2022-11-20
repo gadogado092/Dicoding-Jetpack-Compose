fab8059faa804175b0cc793f42f6ed9153c79a2c
gadogado092 <gadogado092>
2022-11-20 16:16:54 +0800

    add Stateful Converter

--- app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/dicodingjetpackcompose/MainActivity.kt
@@ -3,13 +3,20 @@ package com.example.dicodingjetpackcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.dicodingjetpackcompose.ui.theme.DicodingJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +24,55 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             DicodingJetpackComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                TemperatureApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TemperatureApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        StatefulTemperatureInput()
+    }
 }
+
+@Composable
+fun StatefulTemperatureInput(
+    modifier: Modifier = Modifier
+) {
+    var input by remember { mutableStateOf("") }
+    var output by remember { mutableStateOf("") }
+    Column(modifier.padding(16.dp)) {
+        Text(
+            text = stringResource(id = R.string.stateful_converter),
+            style = MaterialTheme.typography.h5
+        )
+        OutlinedTextField(
+            value = input,
+            label = { Text(stringResource(R.string.enter_celsius)) },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            onValueChange = { newInput ->
+                input = newInput
+                output = convertToFahrenheit(newInput)
+            },
+        )
+        Text(stringResource(R.string.temperature_fahrenheit, output))
+    }
+}
+
+private fun convertToFahrenheit(celsius: String) =
+    celsius.toDoubleOrNull()?.let {
+        (it * 9 / 5) + 32
+    }.toString()
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     DicodingJetpackComposeTheme {
-        Greeting("Android")
+        TemperatureApp()
     }
 }
